3812ae8c35629144df0d96f3daa1bfd8404ca763
Vinicius Ferreira <vinicius.rocha@open-co.com>
2021-08-29 14:48:29 -0300

    handle search not found

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -52,40 +52,46 @@ export function Dashboard() {
   return (
     <Wrapper>
       <Container>
-        <DashboardHeader>
-          <span />
-          <p>Tipo de Transação</p>
-          <p>Data</p>
-          <p>Valor</p>
-        </DashboardHeader>
+        {hasSearchActive && statement.length <= 0 && (
+          <p>Não foram encontrados resultados para a sua busca.</p>
+        )}
 
         {statement.map(({ amountTotal, date, items }) => (
-          <StatementBox key={date}>
-            <StatementDate>{moment(date).format('DD MMMM')}</StatementDate>
+          <>
+            <DashboardHeader>
+              <span />
+              <p>Tipo de Transação</p>
+              <p>Data</p>
+              <p>Valor</p>
+            </DashboardHeader>
 
-            {items.map(
-              ({ actor, dateEvent, amount, entry, source, status }) => (
-                <StatementDetails key={`${actor}${dateEvent}`}>
-                  <div>
-                    <img
-                      src={handleTransactionIcon(status)}
-                      alt="Statement Icon"
-                    />
-                    <h2>{actor}</h2>
-                  </div>
-                  <p>{handleTransaction({ status, source, entry })}</p>
-                  <p>{moment(dateEvent).format('DD MMMM YYYY - HH:mm')}</p>
-                  <p className={handleTransactionValueType(status, entry)}>
-                    {formatReal(amount)}
-                  </p>
-                </StatementDetails>
-              ),
-            )}
+            <StatementBox key={date}>
+              <StatementDate>{moment(date).format('DD MMMM')}</StatementDate>
 
-            <StatementBalance>
-              saldo do dia <span> {formatReal(amountTotal)}</span>
-            </StatementBalance>
-          </StatementBox>
+              {items.map(
+                ({ actor, dateEvent, amount, entry, source, status }) => (
+                  <StatementDetails key={`${actor}${dateEvent}`}>
+                    <div>
+                      <img
+                        src={handleTransactionIcon(status)}
+                        alt="Statement Icon"
+                      />
+                      <h2>{actor}</h2>
+                    </div>
+                    <p>{handleTransaction({ status, source, entry })}</p>
+                    <p>{moment(dateEvent).format('DD MMMM YYYY - HH:mm')}</p>
+                    <p className={handleTransactionValueType(status, entry)}>
+                      {formatReal(amount)}
+                    </p>
+                  </StatementDetails>
+                ),
+              )}
+
+              <StatementBalance>
+                saldo do dia <span> {formatReal(amountTotal)}</span>
+              </StatementBalance>
+            </StatementBox>
+          </>
         ))}
       </Container>
     </Wrapper>
